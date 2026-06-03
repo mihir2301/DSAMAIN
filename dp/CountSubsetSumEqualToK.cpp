4c34f49050511faeeaa46889e6f1c06491891922
mihir2301 <mihirg495423@gmail.com>
2026-06-04 01:01:02 +0530

    case including 0 in subset

--- dp/CountSubsetSumEqualToK.cpp
+++ dp/CountSubsetSumEqualToK.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int dp[1000][1000];
-int dfs(int i,int k,int a[]){
-    if(k==0)return 1;
-    if(i==0)return (a[i]==k);
-    if(dp[i][k]!=-1)return dp[i][k];
+int dfs(int i,int k,int a[]){                        // for 0 
+    if(k==0)return 1;                                // remove this line
+    if(i==0)return (a[i]==k);                       //if(i==0) if(sum==0)&&a[0]==0 return 2;
+    if(dp[i][k]!=-1)return dp[i][k];                //if(sum==a[0])return 1;
     int nottake=dfs(i-1,k,a);
     int take=0;
     if(a[i]<=k) take=dfs(i-1,k-a[i],a);
